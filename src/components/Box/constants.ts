01c6c069259befeb2f45a1faed0cf6807832ec11
DHEERAJ <dheeraj@DHEERAJs-MacBook-Pro.local>
2024-10-04 11:29:34 +0530

    [fix/styles] update the 'Box' and 'Contianer' component styles

--- src/components/Box/constants.ts
+++ src/components/Box/constants.ts
@@ -1,7 +1,7 @@
 export enum BOX_GENERATED_CLASSNAMES {
-  RN_NIX_BOX_SX_CLASS = 'Rn-box-generated-sx-class',
-  RN_NIX_CONTAINER_SX_CLASS = 'Rn-container-generated-sx-class',
-  RN_NIX_CONTAINER_INNER_SX_CLASS = 'Rn-container-generated-inner-sx-class',
+  RN_NIX_BOX_SX_CLASS = 'rn-box-generated-sx',
+  RN_NIX_CONTAINER_SX_CLASS = 'rn-container-generated-sx',
+  RN_NIX_CONTAINER_INNER_SX_CLASS = 'rn-container-generated-inner-sx',
 }
 
 export const XS_MAX_WIDTH = '40%';
