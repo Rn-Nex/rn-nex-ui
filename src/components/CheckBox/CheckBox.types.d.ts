8c275d18ae1f2acdca7ef222a23046eedda7e71b
DHEERAJ <dheeraj@DHEERAJs-MacBook-Pro.local>
2024-11-06 21:42:04 +0530

    [component/check-box] [ ✅ ] Added the size prop for change the size of the check-box component. [ ✅ ] Added the label prop for adding the check box label text. [ ✅ ] Fixed the flex styles of the check box component. [ ✅ ] Users able to click on the full component or only click on the checkbox component.

--- src/components/CheckBox/CheckBox.types.d.ts
+++ src/components/CheckBox/CheckBox.types.d.ts
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableWithoutFeedback, ViewProps, ViewStyle } from 'react-native';
+import { TextStyle, TouchableWithoutFeedback, ViewProps, ViewStyle } from 'react-native';
 import { BoxProps } from '../Box/Box.types';
 import { VariantTypes } from '../../utils';
 
@@ -23,12 +23,6 @@ export interface CheckBoxProps extends React.ComponentPropsWithoutRef<typeof Tou
   isChecked?: boolean;
 
   /**
-   * Boolean value indicating whether the checkbox is in an indeterminate state.
-   * This state is used to represent a mixed selection, often used in hierarchical checkboxes.
-   */
-  isIndeterminate?: boolean;
-
-  /**
    * Color of the checkbox border.
    * Accepts any valid color string.
    */
@@ -41,42 +35,68 @@ export interface CheckBoxProps extends React.ComponentPropsWithoutRef<typeof Tou
   disabled?: boolean;
 
   /**
-   * Image to display when the checkbox is in an indeterminate state.
-   * This can be any React node, such as an icon or an image.
-   */
-  indeterminateImage?: React.ReactNode;
-
-  /**
    * Style object to apply to the wrapper view of the checkbox.
    * Accepts any valid ViewStyle properties.
    */
   checkBoxWrapperStyles?: ViewStyle;
 
   /**
-   * React node to display at the start (left side) of the checkbox.
-   * Commonly used for adding icons or labels.
-   */
-  startAdornment?: React.ReactNode;
-
-  /**
    * Props to apply to the container of the startAdornment.
-   * This omits the 'children' prop from BoxProps to avoid conflicts.
    */
-  startAdornmentContainerProps?: Omit<BoxProps, 'children'>;
+  adornmentContainerStyles?: ViewStyle;
 
   /**
    * React node to display at the end (right side) of the checkbox.
    * Commonly used for adding icons or labels.
    */
-  endAdornment?: React.ReactNode;
+  adornment?: React.ReactNode;
 
   /**
-   * Props to apply to the container of the endAdornment.
-   * This omits the 'children' prop from BoxProps to avoid conflicts.
+   * Different variants of check box component.
    */
-  endAdornmentContainerProps?: Omit<BoxProps, 'children'>;
-
   variant?: VariantTypes;
 
   containerProps?: Omit<ViewProps, 'children'>;
+
+  /**
+   * different sizes of the check boxes.
+   */
+  size?: 'small' | 'medium';
+
+  adornmentType?: 'start' | 'end';
+
+  /**
+   * Optional text label for the component, typically used as the primary description or title.
+   */
+  label?: string;
+
+  /**
+   * Optional styles for customizing the appearance of the main label text.
+   */
+  labelStyles?: TextStyle;
+
+  /**
+   * Optional secondary label for additional descriptive text or subtitle information.
+   */
+  subLabel?: string;
+
+  /**
+   * Optional styles for customizing the appearance of the secondary label text.
+   */
+  subLabelStyles?: TextStyle;
+
+  /**
+   * Optional styles for the container holding the label and subLabel, useful for layout adjustments.
+   */
+  labelContainerStyles?: ViewStyle;
+
+  /**
+   * If true, the component takes up the full width of its container; otherwise, it adjusts based on content.
+   */
+  fullWidth?: boolean;
+
+  /**
+   * Determines the action trigger level; 'root' applies at the top-level, 'element' applies at a specific element level.
+   */
+  actionType?: 'root' | 'element';
 }
