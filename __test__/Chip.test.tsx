43b2ba735e6d99b59c3ac70c1393825c605ffc9a
DHEERAJ <dheeraj@DHEERAJs-MacBook-Pro.local>
2024-11-23 17:14:31 +0530

    [testing/chip/unit] Added test cases for chip component

--- __test__/Chip.test.tsx
+++ __test__/Chip.test.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Chip } from '../src';
+import { Avatar, Chip, green, lightBlue, primary, red, secondary, yellow } from '../src';
 import { SQUARE_BORDER_RADIUS } from '../src/components/Chip/constants';
 import { fireEvent, render } from './test-utils';
+import { View } from 'react-native';
 
 describe('Chip Component', () => {
   const chipMockTestId = 'chip-test-id';
   const chipMockLabel = 'label';
+  const startIconMockTestId = 'start-icon-test-id';
+  const endIconMockTestId = 'end-icon-test-id';
+  const iconMockTestId = 'icon-test-id';
 
+  const mockRef = React.createRef<View>();
   const mockOnPress = jest.fn();
 
   beforeEach(() => {
     jest.clearAllMocks();
+  });
+
+  it('should forward ref correctly', () => {
+    render(<Chip ref={mockRef} />);
+
+    expect(mockRef.current).not.toBeNull();
+    expect(mockRef.current).toBeInstanceOf(View);
   });
 
   it('should match the snapshot with default props', () => {
@@ -52,6 +64,264 @@ describe('Chip Component', () => {
     const { getByTestId } = render(<Chip onPress={mockOnPress} testID={chipMockTestId} />);
     const chip = getByTestId(chipMockTestId);
     fireEvent.press(chip, { nativeEvent: {} });
+    expect(mockOnPress).toHaveBeenCalledTimes(1);
+  });
+
+  it('should change the label default color when passed the labelColor prop', () => {
+    const { getByText } = render(<Chip labelColor={'red'} label={chipMockLabel} onPress={mockOnPress} testID={chipMockTestId} />);
+    const label = getByText(chipMockLabel);
+    expect(label.props.style).toEqual(expect.objectContaining({ color: 'red' }));
+  });
+
+  it('should render with the (secondary) color in (filled) variant', () => {
+    const { getByTestId } = render(<Chip testID={chipMockTestId} />);
+    const chip = getByTestId(chipMockTestId);
+    expect(chip.props.style).toEqual(expect.objectContaining({ backgroundColor: secondary[500] }));
+  });
+
+  it('should render with the (primary) color in (filled) variant', () => {
+    const { getByTestId } = render(<Chip testID={chipMockTestId} color="primary" />);
+    const chip = getByTestId(chipMockTestId);
+    expect(chip.props.style).toEqual(expect.objectContaining({ backgroundColor: primary[500] }));
+  });
+
+  it('should render with the (success) color in (filled) variant', () => {
+    const { getByTestId } = render(<Chip testID={chipMockTestId} color="success" />);
+    const chip = getByTestId(chipMockTestId);
+    expect(chip.props.style).toEqual(expect.objectContaining({ backgroundColor: green[500] }));
+  });
+
+  it('should render with the (error) color in (filled) variant', () => {
+    const { getByTestId } = render(<Chip testID={chipMockTestId} color="error" />);
+    const chip = getByTestId(chipMockTestId);
+    expect(chip.props.style).toEqual(expect.objectContaining({ backgroundColor: red[500] }));
+  });
+
+  it('should render with the (info) color in (filled) variant', () => {
+    const { getByTestId } = render(<Chip testID={chipMockTestId} color="info" />);
+    const chip = getByTestId(chipMockTestId);
+    expect(chip.props.style).toEqual(expect.objectContaining({ backgroundColor: lightBlue[500] }));
+  });
+
+  it('should render with the (warning) color in (filled) variant', () => {
+    const { getByTestId } = render(<Chip testID={chipMockTestId} color="warning" />);
+    const chip = getByTestId(chipMockTestId);
+    expect(chip.props.style).toEqual(expect.objectContaining({ backgroundColor: yellow[500] }));
+  });
+
+  it('should render with the (secondary) color in (outlined) variant', () => {
+    const { getByTestId } = render(<Chip testID={chipMockTestId} variant="outlined" />);
+    const chip = getByTestId(chipMockTestId);
+    expect(chip.props.style).toEqual(expect.objectContaining({ borderColor: secondary[500] }));
+    expect(chip.props.style).not.toHaveProperty('backgroundColor');
+  });
+
+  it('should render with the (primary) color in (outlined) variant', () => {
+    const { getByTestId } = render(<Chip testID={chipMockTestId} color="primary" variant="outlined" />);
+    const chip = getByTestId(chipMockTestId);
+    expect(chip.props.style).toEqual(expect.objectContaining({ borderColor: primary[500] }));
+    expect(chip.props.style).not.toHaveProperty('backgroundColor');
+  });
+
+  it('should render with the (success) color in (outlined) variant', () => {
+    const { getByTestId } = render(<Chip testID={chipMockTestId} color="success" variant="outlined" />);
+    const chip = getByTestId(chipMockTestId);
+    expect(chip.props.style).toEqual(expect.objectContaining({ borderColor: green[500] }));
+    expect(chip.props.style).not.toHaveProperty('backgroundColor');
+  });
+
+  it('should render with the (error) color in (outlined) variant', () => {
+    const { getByTestId } = render(<Chip testID={chipMockTestId} color="error" variant="outlined" />);
+    const chip = getByTestId(chipMockTestId);
+    expect(chip.props.style).toEqual(expect.objectContaining({ borderColor: red[500] }));
+    expect(chip.props.style).not.toHaveProperty('backgroundColor');
+  });
+
+  it('should render with the (info) color in (outlined) variant', () => {
+    const { getByTestId } = render(<Chip testID={chipMockTestId} color="info" variant="outlined" />);
+    const chip = getByTestId(chipMockTestId);
+    expect(chip.props.style).toEqual(expect.objectContaining({ borderColor: lightBlue[500] }));
+    expect(chip.props.style).not.toHaveProperty('backgroundColor');
+  });
+
+  it('should render with the (warning) color in (outlined) variant', () => {
+    const { getByTestId } = render(<Chip testID={chipMockTestId} color="warning" variant="outlined" />);
+    const chip = getByTestId(chipMockTestId);
+    expect(chip.props.style).toEqual(expect.objectContaining({ borderColor: yellow[500] }));
+    expect(chip.props.style).not.toHaveProperty('backgroundColor');
+  });
+
+  it('should sync the chip and label (secondary) color in (outlined) variant', () => {
+    const { getByTestId, getByText } = render(
+      <Chip testID={chipMockTestId} variant="outlined" syncBorderAndLabelColor label={chipMockLabel} />,
+    );
+
+    const chip = getByTestId(chipMockTestId);
+    const label = getByText(chipMockLabel);
+
+    expect(chip.props.style).toEqual(expect.objectContaining({ borderColor: secondary[500] }));
+    expect(label.props.style).toEqual(expect.objectContaining({ color: secondary[500] }));
+    expect(chip.props.style).not.toHaveProperty('backgroundColor');
+  });
+
+  it('should sync the chip and label (primary) color in (outlined) variant', () => {
+    const { getByTestId, getByText } = render(
+      <Chip testID={chipMockTestId} color="primary" variant="outlined" syncBorderAndLabelColor label={chipMockLabel} />,
+    );
+
+    const chip = getByTestId(chipMockTestId);
+    const label = getByText(chipMockLabel);
+
+    expect(chip.props.style).toEqual(expect.objectContaining({ borderColor: primary[500] }));
+    expect(label.props.style).toEqual(expect.objectContaining({ color: primary[500] }));
+    expect(chip.props.style).not.toHaveProperty('backgroundColor');
+  });
+
+  it('should sync the chip and label (success) color in (outlined) variant', () => {
+    const { getByTestId, getByText } = render(
+      <Chip testID={chipMockTestId} variant="outlined" color="success" syncBorderAndLabelColor label={chipMockLabel} />,
+    );
+
+    const chip = getByTestId(chipMockTestId);
+    const label = getByText(chipMockLabel);
+
+    expect(chip.props.style).toEqual(expect.objectContaining({ borderColor: green[500] }));
+    expect(label.props.style).toEqual(expect.objectContaining({ color: green[500] }));
+    expect(chip.props.style).not.toHaveProperty('backgroundColor');
+  });
+
+  it('should sync the chip and label (error) color in (outlined) variant', () => {
+    const { getByTestId, getByText } = render(
+      <Chip testID={chipMockTestId} variant="outlined" color="error" syncBorderAndLabelColor label={chipMockLabel} />,
+    );
+
+    const chip = getByTestId(chipMockTestId);
+    const label = getByText(chipMockLabel);
+
+    expect(chip.props.style).toEqual(expect.objectContaining({ borderColor: red[500] }));
+    expect(label.props.style).toEqual(expect.objectContaining({ color: red[500] }));
+    expect(chip.props.style).not.toHaveProperty('backgroundColor');
+  });
+
+  it('should sync the chip and label (info) color in (outlined) variant', () => {
+    const { getByTestId, getByText } = render(
+      <Chip testID={chipMockTestId} variant="outlined" color="info" syncBorderAndLabelColor label={chipMockLabel} />,
+    );
+
+    const chip = getByTestId(chipMockTestId);
+    const label = getByText(chipMockLabel);
+
+    expect(chip.props.style).toEqual(expect.objectContaining({ borderColor: lightBlue[500] }));
+    expect(label.props.style).toEqual(expect.objectContaining({ color: lightBlue[500] }));
+    expect(chip.props.style).not.toHaveProperty('backgroundColor');
+  });
+
+  it('should sync the chip and label (warning) color in (outlined) variant', () => {
+    const { getByTestId, getByText } = render(
+      <Chip testID={chipMockTestId} variant="outlined" color="warning" syncBorderAndLabelColor label={chipMockLabel} />,
+    );
+
+    const chip = getByTestId(chipMockTestId);
+    const label = getByText(chipMockLabel);
+
+    expect(chip.props.style).toEqual(expect.objectContaining({ borderColor: yellow[500] }));
+    expect(label.props.style).toEqual(expect.objectContaining({ color: yellow[500] }));
+    expect(chip.props.style).not.toHaveProperty('backgroundColor');
+  });
+
+  it('should render correctly with start icon', () => {
+    const { getByTestId, toJSON } = render(
+      <Chip
+        testID={chipMockTestId}
+        startIcon={
+          <Avatar
+            source={{
+              uri: 'https://images.unsplash.com/photo-1568409938619-12e139227838?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            }}
+            size={20}
+            variation="rounded"
+            testID={startIconMockTestId}
+          />
+        }
+      />,
+    );
+
+    const startIcon = getByTestId(startIconMockTestId);
+    expect(startIcon).toBeTruthy();
+    expect(toJSON).toMatchSnapshot();
+  });
+
+  it('should render correctly with end icon', () => {
+    const { getByTestId, toJSON } = render(
+      <Chip
+        testID={chipMockTestId}
+        endIcon={
+          <Avatar
+            source={{
+              uri: 'https://images.unsplash.com/photo-1568409938619-12e139227838?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            }}
+            size={20}
+            variation="rounded"
+            testID={endIconMockTestId}
+          />
+        }
+      />,
+    );
+
+    const endIcon = getByTestId(endIconMockTestId);
+    expect(endIcon).toBeTruthy();
+    expect(toJSON).toMatchSnapshot();
+  });
+
+  it('user should able to click on the start icon', () => {
+    const { getByTestId } = render(
+      <Chip
+        testID={chipMockTestId}
+        startIconProps={{
+          onPress: mockOnPress,
+          testID: iconMockTestId,
+        }}
+        startIcon={
+          <Avatar
+            source={{
+              uri: 'https://images.unsplash.com/photo-1568409938619-12e139227838?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            }}
+            size={20}
+            variation="rounded"
+            testID={startIconMockTestId}
+          />
+        }
+      />,
+    );
+
+    const startIconTouch = getByTestId(iconMockTestId);
+    fireEvent.press(startIconTouch, { nativeEvent: {} });
+    expect(mockOnPress).toHaveBeenCalledTimes(1);
+  });
+
+  it('user should able to click on the end icon', () => {
+    const { getByTestId } = render(
+      <Chip
+        testID={chipMockTestId}
+        endIconProps={{
+          onPress: mockOnPress,
+          testID: iconMockTestId,
+        }}
+        endIcon={
+          <Avatar
+            source={{
+              uri: 'https://images.unsplash.com/photo-1568409938619-12e139227838?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            }}
+            size={20}
+            variation="rounded"
+            testID={startIconMockTestId}
+          />
+        }
+      />,
+    );
+
+    const endIconTouch = getByTestId(iconMockTestId);
+    fireEvent.press(endIconTouch, { nativeEvent: {} });
     expect(mockOnPress).toHaveBeenCalledTimes(1);
   });
 });
